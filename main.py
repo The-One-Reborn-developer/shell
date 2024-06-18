3420c721d0b72f93fead88973df35a70788bb3a5
Adil Arshidinov <mortonmarrion@gmail.com>
2024-06-18 17:19:58 +0300

    feat: REPL

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import sys
 
 def main():
-    # Print standard shell prompt
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
-
     # List to store commands
     commands_list = []
-    # Read user input
-    user_input = input()
 
-    # If the command that user has inputed is not in the list of commands
-    if user_input not in commands_list:
-        print(f'{user_input}: command not found')    
+    while True:
+        # Print standard shell prompt
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+        
+        # Read user input
+        user_input = input()
+
+        # If the command that user has inputed is not in the list of commands
+        if user_input not in commands_list:
+            print(f'{user_input}: command not found')    
 
 if __name__ == "__main__":
     main()
