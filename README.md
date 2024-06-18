33a15818ea89ed859bd9af7f54e28e9164a341a8
Adil Arshidinov <mortonmarrion@gmail.com>
2024-06-18 18:51:41 +0300

    feat: exit command with 0 status

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Feats: REPL
+<h2>FEATS:</h2>
+* REPL
+
+<h2>COMMANDS:</h2>
+* exit
--- main.py
+++ main.py
@@ -2,19 +2,36 @@ import sys
 
 def main():
     # List to store commands
-    commands_list = []
-
+    commands_list = ['exit']
+    # List to store arguments
+    arguments_list = ['0']
+    # Exit status
+    exit_status = 0
+    
     while True:
-        # Print standard shell prompt
+        # Print the standard shell prompt
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        
-        # Read user input
-        user_input = input()
 
-        # If the command that user has inputed is not in the list of commands
-        if user_input not in commands_list:
-            print(f'{user_input}: command not found')    
+        # Wait for user input and split it into a list of command and command's arguments
+        user_input = input().split()
+        
+        # If the inputed command is not in the list of commands
+        if user_input[0] not in commands_list:
+            print(f'{user_input[0]}: command not found')
+        else:
+            if user_input[0] == commands_list[0]:  # If the inputed command is exit
+                if len(user_input) > 1:  # If the inputed exit command was with arguments
+                      if user_input[1] == arguments_list[0]:  # If the inputed arguments is 0
+                            exit_status = 0
+                            break
+                else:  # If the inputed exit command was without arguments
+                    argument = input()  # Wait for argument input
+                    if argument == arguments_list[0]:  # If the inputed argument is 0
+                        exit_status = 0
+                        break
+        
+    return sys.exit(exit_status)
 
 if __name__ == "__main__":
     main()
