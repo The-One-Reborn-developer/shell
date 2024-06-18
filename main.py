408a2251e178f014657f9fec5cb8a30f2f83f127
Adil Arshidinov <mortonmarrion@gmail.com>
2024-06-18 16:51:31 +0300

    feat: shell writes a $ prompt and waits for user input

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+
+def main():
+    # Print standard shell prompt
+    sys.stdout.write("$ ")
+    sys.stdout.flush()
+
+    # Read user input
+    input()
+
+if __name__ == "__main__":
+    main()
