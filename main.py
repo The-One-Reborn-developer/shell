0b2798e16e6a975b194ce1db05f9691885de7866
Adil Arshidinov <mortonmarrion@gmail.com>
2024-06-18 17:03:12 +0300

    feat: handling for nonexistant commands

--- main.py
+++ main.py
@@ -5,8 +5,14 @@ def main():
     sys.stdout.write("$ ")
     sys.stdout.flush()
 
+    # List to store commands
+    commands_list = []
     # Read user input
-    input()
+    user_input = input()
+
+    # If the command that user has inputed is not in the list of commands
+    if user_input not in commands_list:
+        print(f'{user_input}: command not found')    
 
 if __name__ == "__main__":
     main()
