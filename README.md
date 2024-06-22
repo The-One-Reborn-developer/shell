bf0728ce0a8e466688985a10e6f7796586ce7cf2
Adil Arshidinov <mortonmarrion@gmail.com>
2024-06-22 09:39:59 +0300

    feat: program execution

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 1. exit (with status 0): terminates the program with the argument status.<br>
 2. echo: returns the arguments to STDOUT.<br>
 3. type: checks whether the command is a builtin shell command or a command from a given variable PATH or not a command.<br>
+4. program execution: when PATH, program name and arguments are given searches for the program in PATH and executes it with the given arguments.<br>
--- main.py
+++ main.py
@@ -1,63 +1,101 @@
 import sys
 import os
+import subprocess
 
 def main():
-    # List to store commands
-    commands_list = ['exit', 'echo', 'type']
-    # List to store arguments
-    arguments_list = ['0']
-    # Exit status
-    exit_status = 0
+    commands_list = ['exit', 'echo', 'type']  # List to store commands
+    arguments_list = ['0']  # List to store arguments
+    exit_status = 0  # Exit status
     
     while True:
-        # Write a standard shell prompt to stdout
-        sys.stdout.write("$ ")
+        sys.stdout.write("$ ")  # Write a standart shell prompt to stdout
         sys.stdout.flush()
 
         # Wait for user input and split it into a list of command and command's arguments
         user_input = input().strip().split()
-        # If the inputed command is not in the list of commands
-        if user_input[0] not in commands_list:
-            print(f'{user_input[0]}: command not found')
-        else:
-            if user_input[0] == commands_list[0]:  # If the inputed command is exit
-                if len(user_input) > 1:  # If the inputed exit command was with arguments
-                      if user_input[1] == arguments_list[0]:  # If the inputed arguments is 0
-                            exit_status = 0
-                            return sys.exit(exit_status)
-                else:  # If the inputed exit command was without arguments
-                    argument = input()  # Wait for argument input
-                    if argument == arguments_list[0]:  # If the inputed argument is 0
-                        exit_status = 0
-                        return sys.exit(exit_status)
-            elif user_input[0] == commands_list[1]:  # If the inputed command is echo
-                if len(user_input) > 1:  # If the inputed echo command was with arguments
-                    print(' '.join(user_input[1:]))  # Echo the arguments
-                else:  # If the inputed echo command was without arguments
-                    argument = input()  # Wait for argument input
-                    print(argument)
-            elif user_input[0] == commands_list[2]:  # If the inputed command is type
-                if len(user_input) > 1:  # If the inputed type command was with arguments
-                    if user_input[1] in commands_list:  # If the inputed argument is a command
-                        print(f'{user_input[1]} is a shell builtin')
+
+        if not user_input:  # If the user didn't input anything
+            continue
+
+        if user_input[0] == commands_list[0]:  # If the inputed command is exit
+            if len(user_input) > 1:  # If the inputed exit command was with arguments
+                if user_input[1] == arguments_list[0]:  # If the inputed arguments is 0
+                    exit_status = 0
+                    return sys.exit(exit_status)
+            else:  # If the inputed exit command was without arguments
+                argument = input()  # Wait for argument input
+                if argument == arguments_list[0]:  # If the inputed argument is 0
+                    exit_status = 0
+                    return sys.exit(exit_status)
+        elif user_input[0] == commands_list[1]:  # If the inputed command is echo
+            if len(user_input) > 1:  # If the inputed echo command was with arguments
+                print(' '.join(user_input[1:]))  # Echo the arguments
+            else:  # If the inputed echo command was without arguments
+                argument = input()  # Wait for argument input
+                print(argument)
+        elif user_input[0] == commands_list[2]:  # If the inputed command is type
+            if len(user_input) > 1:  # If the inputed type command was with arguments
+                if user_input[1] in commands_list:  # If the inputed argument is a command
+                    print(f'{user_input[1]} is a shell builtin')
+                elif user_input[1] not in commands_list:
+                    find_command_result = find_command(user_input[1])
+                    if find_command_result:
+                        print(f'{user_input[1]} is {find_command_result}')
                     else:
                         print(f'{user_input[1]}: not found')
+        elif find_command(user_input[0]):  # If the input is a program that is in PATH
+            execute_program(user_input[0], user_input[1:])
+        else:
+            print(f'{user_input[0]}: command not found')
 
 
-# Function to find a command that is in PATH
 def find_command(command):
-    command_found = False
+    '''
+    Function to find a command that is in PATH
+
+    Parameters
+    ----------
+    command : str
+        The name of the command
+
+    Returns
+    -------
+    str
+        The path of the command
+    '''
     path_dirs = os.environ.get('PATH', '').split(os.pathsep)
     for directory in path_dirs:  # Check each directory in PATH
         for root, dirs, files in os.walk(directory):  # Check each subdirectory in the directory
             for name in files:  # Check each file in the directory
                 if name == command:
-                    command_found = True
-                    print(f'{command} is {os.path.abspath(os.path.join(root, name))}')
-                    return
+                    return os.path.abspath(os.path.join(root, name))
+    
+    return None
 
-    if not command_found:
-        print(f'{command}: not found')
+
+def execute_program(program, arguments):
+    '''
+    Function to execute a program
+
+    Parameters
+    ----------
+    program : str
+        The name of the program
+    arguments : list
+        The arguments to pass to the program
+
+    Returns
+    -------
+    None
+    '''
+    program_path = find_command(program)  # Find the path of the program
+    if program_path:
+        try:
+            result = subprocess.run([program_path] + arguments)
+        except Exception as RunError:
+            print(f'Error executing {program}: {RunError}')
+    else:
+        print(f'{program}: command not found')
 
 
 if __name__ == "__main__":
